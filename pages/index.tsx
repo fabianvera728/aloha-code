c53f5886742bac38781c5bcf6ed69be01a7cbba9
FabianVC <fabianvera728@gmail.com>
2022-11-19 13:41:12 -0500

    [add] requirements of project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
     return (
-        <div className="bg-gray-200">
+        <div className="bg-gray-200 flex">
             <h2> Aloha code </h2>
+            <Image src="/images/prueba.jpg" width={500} height={500} alt='Prueba'/>
         </div>
     )
 }
